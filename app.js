eaa658963d9b9840cbd16e831210adbc2952e964
fahrurrazi <evilkyro1965@yahoo.com>
2015-04-16 09:25:49 +0700

    adding scroll and highlight

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ var comboDemoApp = angular.module("comboDemoApp",[]);
 comboDemoApp.controller("comboDemoController", function($scope){
 	$scope.data = [
 					{'value':'en','label':'English'},
-					{'value':'ina','label':'Indonesia'}
+					{'value':'ina','label':'Indonesia'},
+					{'value':'farsi','label':'Farsi'},
+					{'value':'arab','label':'Arab'},
+					{'value':'en','label':'English'},
+					{'value':'ina','label':'Indonesia'},
+					{'value':'farsi','label':'Farsi'},
+					{'value':'arab','label':'Arab'},
+					{'value':'en','label':'English'},
+					{'value':'ina','label':'Indonesia'},
+					{'value':'farsi','label':'Farsi'},
+					{'value':'arab','label':'Arab'},
+					{'value':'ina','label':'Indonesia'},
+					{'value':'farsi','label':'Farsi'},
+					{'value':'arab','label':'Arab'},
+					{'value':'en','label':'English'},
+					{'value':'ina','label':'Indonesia'},
+					{'value':'farsi','label':'Farsi'},
+					{'value':'arab','label':'Arab'}
 				];
 	$scope.language = {};			
 
@@ -45,6 +62,9 @@ comboDemoApp.directive('combobutton', function () {
 						if($(options.id).hasClass('opened')==false) {
 							$(options.id).addClass('opened')
 							$(options.id).css("display","block");
+							if( typeof scope.selected.index !== "undefined") {
+								$(options.id).scrollTop( scope.selected.scrollTop );
+							}
 						}
 						else {
 							$(options.id).removeClass('opened')
@@ -52,11 +72,31 @@ comboDemoApp.directive('combobutton', function () {
 						}
 					});
 					
+					$(document).mouseup(function (e){
+					
+							var container = $(comboModalId);
+
+							if (!container.is(e.target) 
+									&& container.has(e.target).length === 0) 
+							{
+								container.find(".jqueryComboModal").removeClass('opened');
+								container.find(".jqueryComboModal").hide();
+							}
+
+					});
+					
 					$(options.id+" .comboItem").on('click',function(event){
 						var target = $(event.target);
 						scope.selected.label = target.html();
 						scope.selected.value = target.attr('value');
+						var optionsWrapper = $(options.id);
+						var index = $(options.id+" .comboItem").index( target );
+						scope.selected.index = index;
+						scope.selected.scrollTop = $(options.id).scrollTop();
+						
 						var selected = scope.selected;
+						$(options.id+" .comboItem").removeClass("selected");
+						target.addClass("selected");
 						$(comboModalId).data("selected",scope.selected);
 						$(comboModalId).find(".comboLabel").html(scope.selected.label);
 						scope.$apply(function () {
