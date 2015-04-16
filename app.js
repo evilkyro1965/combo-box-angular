7e2de0b521c839085b197f69bed3f4e3e8f17666
fahrurrazi <evilkyro1965@yahoo.com>
2015-04-16 09:49:52 +0700

    adding feature scroll and highlight

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var comboDemoApp = angular.module("comboDemoApp",[]);
+var comboDemoApp = angular.module("comboDemoApp",['ComboModal']);
 
 comboDemoApp.controller("comboDemoController", function($scope){
 	$scope.data = [
@@ -24,88 +24,5 @@ comboDemoApp.controller("comboDemoController", function($scope){
 				];
 	$scope.language = {};			
 
-});
-
-comboDemoApp.directive('combobutton', function () {
-    return {
-        scope : {
-            data : "=data",
-            selected : "=selected"
-        },   
-        link: function (scope, element, attrs) {
-          var counterId = 1;
-
-					var options = {
-						data : new Array(),
-						modalDiv : 
-							"<div class='jqueryComboModal'>"+
-							"</div>",
-						id : ""
-					};
-					
-					var comboModalId = "#"+element.attr('id');
-			
-					var data = scope.data;
-					if(data.length>0){
-						var id = "#jqueryComboModal"+counterId;
-						element.append("<div id='jqueryComboModal"+counterId+"' class='jqueryComboModal'></div>");
-						$(id).css("top",element.height()+1);
-						$(id).css("min-width",element.outerWidth()-3);
-						options.id = id;
-						
-						for(var i=0;i<data.length;i++) {
-							$(options.id).append("<div class='comboItem' value='"+data[i].value+"'>"+data[i].label+"</div>");
-						}
-					}
-					
-					element.on('click',function(e){
-						if($(options.id).hasClass('opened')==false) {
-							$(options.id).addClass('opened')
-							$(options.id).css("display","block");
-							if( typeof scope.selected.index !== "undefined") {
-								$(options.id).scrollTop( scope.selected.scrollTop );
-							}
-						}
-						else {
-							$(options.id).removeClass('opened')
-							$(options.id).css("display","none");
-						}
-					});
-					
-					$(document).mouseup(function (e){
-					
-							var container = $(comboModalId);
-
-							if (!container.is(e.target) 
-									&& container.has(e.target).length === 0) 
-							{
-								container.find(".jqueryComboModal").removeClass('opened');
-								container.find(".jqueryComboModal").hide();
-							}
-
-					});
-					
-					$(options.id+" .comboItem").on('click',function(event){
-						var target = $(event.target);
-						scope.selected.label = target.html();
-						scope.selected.value = target.attr('value');
-						var optionsWrapper = $(options.id);
-						var index = $(options.id+" .comboItem").index( target );
-						scope.selected.index = index;
-						scope.selected.scrollTop = $(options.id).scrollTop();
-						
-						var selected = scope.selected;
-						$(options.id+" .comboItem").removeClass("selected");
-						target.addClass("selected");
-						$(comboModalId).data("selected",scope.selected);
-						$(comboModalId).find(".comboLabel").html(scope.selected.label);
-						scope.$apply(function () {
-							scope.selected = selected;
-						});
-					});
-					counterId++;
-							
-        }
-    };
 });
 
--- combo-modal.js
+++ combo-modal.js
@@ -1,69 +1,85 @@
-(function($) {
-	counterId = 1;
-	
-	$.fn.comboModal = function( options ) {
-		
-		$(this).each(function(){  
-			var comboModalId = "#"+$(this).attr('id');
-			
-			options = $.extend({}, $.fn.comboModal.defaultOptions, options);
-			var data = options.data;
-			if(data.length>0){
-				var id = "#jqueryComboModal"+counterId;
-				$(comboModalId).append("<div id='jqueryComboModal"+counterId+"' class='jqueryComboModal'></div>");
-				$(id).css("top",$(this).height()+1);
-				$(id).css("min-width",$(this).outerWidth()-3);
-				options.id = id;
-				
-				for(var i=0;i<data.length;i++) {
-					$(options.id).append("<div class='comboItem' value='"+data[i].value+"'>"+data[i].label+"</div>");
-				}
-			}
-			
-			$(this).on('click',function(e){
-				if($(options.id).hasClass('opened')==false) {
-					$(options.id).addClass('opened')
-					$(options.id).css("display","block");
-				}
-				else {
-					$(options.id).removeClass('opened')
-					$(options.id).css("display","none");
-				}
-			});
-			
-			$(document).mouseup(function (e){
-			
-					var container = $(comboModalId+" "+".jqueryComboModal");
+var comboModal = angular.module("ComboModal",[]);
 
-					if (!container.is(e.target) 
-							&& container.has(e.target).length === 0) 
-					{
-						container.removeClass('opened')
-						container.hide();
+comboModal.directive('combobutton', function () {
+    return {
+        scope : {
+            data : "=data",
+            selected : "=selected"
+        },   
+        link: function (scope, element, attrs) {
+          var counterId = 1;
+
+					var options = {
+						data : new Array(),
+						modalDiv : 
+							"<div class='jqueryComboModal'>"+
+							"</div>",
+						id : ""
+					};
+					
+					var comboModalId = "#"+element.attr('id');
+			
+					var data = scope.data;
+					if(data.length>0){
+						var id = "#jqueryComboModal"+counterId;
+						element.append("<div id='jqueryComboModal"+counterId+"' class='jqueryComboModal'></div>");
+						$(id).css("top",element.height()+1);
+						$(id).css("min-width",element.outerWidth()-3);
+						options.id = id;
+						
+						for(var i=0;i<data.length;i++) {
+							$(options.id).append("<div class='comboItem' value='"+data[i].value+"'>"+data[i].label+"</div>");
+						}
 					}
 					
-			});
-			
-			$(options.id+" .comboItem").on('click',function(event){
-				var target = $(event.target);
-				options.selected.label = target.html();
-				options.selected.value = target.attr('value');
-				$(comboModalId).data("selected",options.selected);
-				$(comboModalId).find(".comboLabel").html(options.selected.label);
-			});
-			counterId++;
-			
-		});		
+					element.on('click',function(e){
+						if($(options.id).hasClass('opened')==false) {
+							$(options.id).addClass('opened')
+							$(options.id).css("display","block");
+							if( typeof scope.selected.index !== "undefined") {
+								$(options.id).scrollTop( scope.selected.scrollTop );
+							}
+						}
+						else {
+							$(options.id).removeClass('opened')
+							$(options.id).css("display","none");
+						}
+					});
+					
+					$(document).mouseup(function (e){
+					
+							var container = $(comboModalId);
 
-	};
-			 
-	$.fn.comboModal.defaultOptions = {
-		data : new Array(),
-		modalDiv : 
-			"<div class='jqueryComboModal'>"+
-			"</div>",
-		selected : {},
-		id : ""
-	};
-		
-}(jQuery));
+							if (!container.is(e.target) 
+									&& container.has(e.target).length === 0) 
+							{
+								container.find(".jqueryComboModal").removeClass('opened');
+								container.find(".jqueryComboModal").hide();
+							}
+
+					});
+					
+					$(options.id+" .comboItem").on('click',function(event){
+						var target = $(event.target);
+						scope.selected.label = target.html();
+						scope.selected.value = target.attr('value');
+						var optionsWrapper = $(options.id);
+						var index = $(options.id+" .comboItem").index( target );
+						scope.selected.index = index;
+						scope.selected.scrollTop = $(options.id).scrollTop();
+						
+						var selected = scope.selected;
+						$(options.id+" .comboItem").removeClass("selected");
+						target.addClass("selected");
+						$(comboModalId).data("selected",scope.selected);
+						$(comboModalId).find(".comboLabel").html(scope.selected.label);
+						scope.$apply(function () {
+							scope.selected = selected;
+						});
+					});
+					counterId++;
+							
+        }
+    };
+});
+
